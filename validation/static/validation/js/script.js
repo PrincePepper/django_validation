fea9c2e6aec7082be45724f082e978bdba2ba309
PrincePepper <semyon3336@gmail.com>
2020-12-19 15:50:48 +1000

    #start commit

--- validation/static/validation/js/script.js
+++ validation/static/validation/js/script.js
@@ -1,22 +1,64 @@
-for (let temp = 2020; temp >= 1980; temp--) {
-    $('#year').append('<option value="">' + temp + '</option>');
-}
-
 function toggle_visibility(id, boolen) {
-
-    var e = document.getElementById(id);
+    let e = document.getElementById(id);
     if (boolen === true)
         e.style.display = 'none';
     else
         e.style.display = 'block';
 }
 
-function next_step() {
+function next_step(id_old, id_next) {
 
+    let form = document.getElementsByClassName('needs-validation');
+    Array.prototype.filter.call(form, function () {
+        jQuery.validator.addMethod("selectnic", function (value, element) {
+            return !/^[0-9]{9}[vVxX]$/.test(value);
+        }, "wrong nic number");
+
+        let temp_valid = $(".validate").validate({
+            rules: {
+                surname: {
+                    required: true,
+                    minlength: 2,
+                    digits: false,
+                    selectnic: true
+                },
+                surname: {
+                    required: true,
+                    minlength: 2,
+                    digits: false,
+                    selectnic: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please specify your name",
+                    selectnic: "22222222222222",
+                },
+                surname: {
+                    required: "We need your email address to contact you",
+                    digits: "111111111",
+
+                },
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                }
+            }
+        })
+        if (temp_valid.errorList.length === 0)
+            console.log(1111)
+        console.log(temp_valid)
+    });
+
+    // toggle_visibility(id_old, true)
+    // toggle_visibility(id_next, false)
 }
 
-function pushdata() {
-    for (let temp = 2020; temp >= 1980; temp--) {
-        $('#year').append('<option value="">' + temp + '</option>');
-    }
+function return_step(id_new, id_old) {
+    toggle_visibility(id_new, true)
+    toggle_visibility(id_old, false)
 }
