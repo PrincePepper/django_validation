bdf6c3522a08522f2804ef2601939d2f76642ae5
PrincePepper <semyon3336@gmail.com>
2020-12-20 03:24:57 +1000

    #start commit

--- validation/static/validation/js/script.js
+++ validation/static/validation/js/script.js
@@ -1,3 +1,5 @@
+let validator;
+
 function toggle_visibility(id, boolen) {
     let e = document.getElementById(id);
     if (boolen === true)
@@ -6,61 +8,54 @@ function toggle_visibility(id, boolen) {
         e.style.display = 'block';
 }
 
+
 function next_step(id_old, id_next) {
+    let a1 = $("#name").valid()
+    let a2 = $("#surname").valid()
+    let a3 = $("#email").valid()
+    if (a1 && a2 && a3) {
+        toggle_visibility(id_old, true)
+        toggle_visibility(id_next, false)
+    }
 
-    let form = document.getElementsByClassName('needs-validation');
-    Array.prototype.filter.call(form, function () {
-        jQuery.validator.addMethod("letteronly", function (value, element) {
-            let regex = new RegExp("[.,\/#!$%\^&\*;:{}=\-_`~()0-9]+$");
-            let key = value;
-            return regex.test(key);
-
-        }, "please use only alphabetic characters");
-
-        let temp_valid = $(".validate").validate({
-            rules: {
-                surname: {
-                    required: true,
-                    minlength: 2,
-                    digits: false,
-                    letteronly: true,
-                },
-                surname: {
-                    required: true,
-                    minlength: 2,
-                    digits: false,
-                },
-                email: {
-                    required: true,
-                    email: true
-                }
-            },
-            messages: {
-                name: {
-                    required: "Please specify your name",
-                    letteronly: "22222222222222",
-                },
-                surname: {
-                    required: "We need your email address to contact you",
-                    digits: "111111111",
-
-                },
-                email: {
-                    required: "We need your email address to contact you",
-                    email: "Your email address must be in the format of name@domain.com"
-                }
-            }
-        })
-        if (temp_valid.errorList.length === 0)
-            console.log(1111)
-        console.log(temp_valid)
-    });
-
-    // toggle_visibility(id_old, true)
-    // toggle_visibility(id_next, false)
 }
 
 function return_step(id_new, id_old) {
     toggle_visibility(id_new, true)
     toggle_visibility(id_old, false)
+}
+
+function submit_step() {
+    let a1 = $("#career").valid()
+    let a2 = $("#description").valid()
+    let a3 = $("#password").valid()
+    let a4 = $("#passwordConfirm").valid()
+    if (a1 && a2 && a3 && a4) {
+        console.group("Form second")
+        console.log('Вы успешно зарегистрированны!')
+        console.log('Ваше имя: ' + $("#name").val())
+        console.log('Ваша фамилия: ' + $("#surname").val())
+        console.log('Email: ' + $("#email").val())
+        console.log('Год рождения: ' + $("#career").val())
+        console.log('Профессия: ' + $("#description").val())
+        console.log('Пароль: ' + $("#passwordConfirm").val())
+        console.groupEnd()
+        reset()
+    }
+}
+
+function reset() {
+    // сбросить шаг и показать сообщение о регистрации
+    toggle_visibility('step-2', true)
+    toggle_visibility('step-1', false)
+    toggle_visibility('alert', false)
+    // убрать сообщение о регистрации
+    setTimeout(() => {
+        toggle_visibility('alert', true)
+    }, 1500)
+    // сбросить все поля
+    $('.validate')[0].reset();
+    // сбросить валидацию
+
+    validator.resetForm()
 }
