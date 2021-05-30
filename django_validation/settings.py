6787ff72ecac930ffa6258436928fcd7da7398c6
Semen <43268369+PrincePepper@users.noreply.github.com>
2021-05-30 15:47:50 +1000

    Update settings.py

--- django_validation/settings.py
+++ django_validation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yiqk)+xp1gw1(btq+tj^^wx$lr7f&=5qbdw@l_c)%1rzy$v1)!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ssereda.herokuapp']
+ALLOWED_HOSTS = ['ssereda.herokuapp.com']
 
 # Application definition
 
