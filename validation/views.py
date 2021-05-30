e4214e286aa8808da734e1e6d233a127058d6749
Semen <43268369+PrincePepper@users.noreply.github.com>
2021-05-30 15:38:49 +1000

    Update views.py

--- validation/views.py
+++ validation/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'validation/main.html')
+    return render(request, 'validation/index.html')
